0711573e3e3cc85967d51af31003194c3b4211ac
FueledByBesh <olzhasakimbai@gmail.com>
2026-05-07 12:40:15 +0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
 
-    print!("{}: command not found\n", input.trim());
-    print!("$")
+        print!("{}: command not found\n", input.trim());
+    }
 }
