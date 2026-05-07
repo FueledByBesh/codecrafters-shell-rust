d9d2982b964d21c910d0c235703a1e5c2866ac52
FueledByBesh <olzhasakimbai@gmail.com>
2026-05-07 13:25:05 +0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-
 fn main() {
+    const SUPPORTED_COMMANDS: [&str; 3] = ["type","echo","exit"];
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Failed to read line");
-        let input_trim = input.trim();
-        if input_trim == "exit" {
+        let command = input.trim();
+        let command_vec: Vec<&str> = command.split_whitespace().collect();
+        if command_vec[0] == "exit" {
             break;
-        }
-        let command: Vec<&str> = input_trim.split_whitespace().into_iter().collect();
-        if command[0] == "echo"{
-            println!("{}", &command[1..].join(" "));
+        } else if command_vec[0] == "type"{
+            if command_vec.len() < 2 {
+                println!("type: missing operand");
+                continue;
+            }else if command_vec.len() > 2 {
+                println!("type: too many arguments");
+                continue;
+            }
+            if SUPPORTED_COMMANDS.contains(&command_vec[1]){
+                println!("{}: is a shell builtin", command_vec[1]);
+            }else{
+                println!("{}: command not found", command_vec[1]);
+            }
+        } else if command_vec[0] == "echo"{
+            println!("{}", &command[5..]);
         }else {
-            print!("{}: command not found\n", input_trim);
+            print!("{}: command not found\n", command_vec[0]);
         }
 
     }
