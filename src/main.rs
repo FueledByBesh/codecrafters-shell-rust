393ca4698b816cdbfbdaad2c16223f1fda2709fb
FueledByBesh <olzhasakimbai@gmail.com>
2026-05-10 07:35:27 +0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -13,29 +13,37 @@ fn main() {
         io::stdin().read_line(&mut input).expect("Failed to read line");
         let command = input.trim();
         let command_vec: Vec<&str> = command.split_whitespace().collect();
-        if command_vec[0] == "exit" {
-            break;
-        } else if command_vec[0] == "type"{
-            if command_vec.len() < 2 {
-                println!("type: missing operand");
-                continue;
-            }else if command_vec.len() > 2 {
-                println!("type: too many arguments");
-                continue;
-            }
-            if BUILTIN_COMMANDS.contains(&command_vec[1]){
-                println!("{} is a shell builtin", command_vec[1]);
-            }else{
-                if let Some(x) = find_executable_from_path(&command_vec[1]) {
-                    println!("{} is {}",command_vec[1], x);
-                } else {
-                    println!("{}: not found", command_vec[1])
+        if BUILTIN_COMMANDS.contains(&command_vec[0]){
+            if command_vec[0] == "exit" {
+                break;
+            } else if command_vec[0] == "type"{
+                if command_vec.len() < 2 {
+                    println!("type: missing operand");
+                    continue;
+                }else if command_vec.len() > 2 {
+                    println!("type: too many arguments");
+                    continue;
                 }
+                if BUILTIN_COMMANDS.contains(&command_vec[1]){
+                    println!("{} is a shell builtin", command_vec[1]);
+                }else{
+                    if let Some(x) = find_executable_from_path(&command_vec[1]) {
+                        println!("{} is {}",command_vec[1], x);
+                    } else {
+                        println!("{}: not found", command_vec[1])
+                    }
+                }
+            } else if command_vec[0] == "echo"{
+                println!("{}", &command[5..]);
+            }else {
+                panic!("{}: unhandled builtin command!", command_vec[0])
             }
-        } else if command_vec[0] == "echo"{
-            println!("{}", &command[5..]);
-        }else {
-            print!("{}: command not found\n", command_vec[0]);
+        } else {
+            if let Some(x) = find_executable_from_path(command_vec[0]){
+                execute( &x, &command_vec[1..])
+            }else {
+                print!("{}: command not found\n", command_vec[0]);
+            }
         }
 
     }
@@ -59,4 +67,10 @@ use std::os::unix::fs::PermissionsExt;
 #[cfg(unix)]
 fn is_executable(path: &PathBuf)->bool{
     fs::metadata(path).map(|m| m.is_file() && m.permissions().mode() & 0o111 !=0).unwrap_or(false)
+}
+//trusts that the file is executable
+use std::process::Command;
+fn execute(command: &str, args: &[&str]){
+    let output = Command::new(command).args(args).output().unwrap();
+    print!("{}",String::from_utf8_lossy(&output.stdout))
 }
